7fd82103143fa9839de8ddf57381b226ea004360
Leon Emil <LeonEmilioBernal@gmail.com>
2019-06-15 19:49:21 -0300

    initial commit

---
+++ footer.php
@@ -0,0 +1,100 @@
+<!-- Pie de pagina / Footer page -->
+    <footer class="footer container-fluid bg-primary text-white mt-auto pt-5 pb-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <h3 class="titulosDelFooter">Lab<strong>TIC</strong>- UNITEC</h3>
+                    <p>Departamento de Electrónica
+                        <br>Facultad de Ingeniería
+                        <br>Universidad Nacional de La Plata
+                        <br>
+                        <br>Calle 116 y 48, 2º Piso, La Plata
+                        <br>Teléfono: +54 221 422-7628
+                    </p>
+                    <ul class="listaDeRedesSociales" id="redesSociales">
+                        <li>
+                            <a href="https://www.facebook.com/uid.unitec/">
+                                <i class="fa fa-2x fa-facebook-square fa-fw text-inverse iconoRedesSociales"></i>
+                                <span class="descripcionLectorDePantalla descripcionLectorDePantallaFocus">Ir a facebook unitec</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/labtic-unitec">
+                                <i class="fa fa-2x fa-github-square fa-fw text-inverse iconoRedesSociales"></i>
+                                <span class="descripcionLectorDePantalla descripcionLectorDePantallaFocus">Ir a Github de labtic unitec</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-2x fa-rss-square fa-fw text-inverse iconoRedesSociales"></i>
+                                <span class="descripcionLectorDePantalla descripcionLectorDePantallaFocus">Suscribirse a este sitio web por RSS</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <hr class="d-xl-none d-sm-block d-md-none">
+                </div>
+                <div class="col-12 col-md-4">
+                    <h3 class="titulosDelFooter">Como llegar</h3>
+                    <p>
+                        Camino accesible con rampas y ascensor.
+                        <br>
+                        Entrada desde calle 1 y 48, facultad de ciencias exactas.
+                    </p>
+                    <p class="descripcionLectorDePantalla descripcionLectorDePantallaFocus">
+                        En el siguiente video se ve como llegar al laboratorio desde la entrada de la facultad de ciencias exactas en calle 1 y 48.
+                        En el video se muestra como llegar por un camino accesible con rampas y asensor. El lugar es un predio con varios edificios y jardines que se 
+                        deben atravezar para poder llegar al laboratorio ubicado en calle 116 y 48, segundo piso.
+                        Para llegar al laboratorio desde aqui se debe entrar desde esta entrada en 1 y 48 y caminar 100 metros derecho hasta llegar a la facultad de electrotecnia.
+                        Una vez alli se puede entrar a través de escaleras o rampas que estan detras de la entrada principal del edificio.
+                        El laboratorio esta ubicado en el segundo piso al cual se puede llegar por ascensor o escaleras.
+                        Para llegar al ascensor se debe llegar al hall principal a traves de los pasillos, el asensor se encuentra en el pasillo derecho del hall principal
+                        y tiene inscripción en braille en su puerta para llamar al ascensor y un sonido que indica cuando llega a nuestro piso al llamarlo.
+                        Los botones del ascensor tambien tienen inscripcion en braille.
+                        Marcamos el botón para ir al segundo piso.
+                        Al llegar al segundo piso el ascensor emite un sonido.
+                        Estando en el segundo piso al abrir la puerta del ascensor estaremos en justo frente de la puerta del laboratorio que tiene una inscripción en
+                        braille y un timbre.
+                        Se recomienda contactar con el laboratorio por telefono o mail antes de ir personalmente.
+                    </p>
+                    <div class="video">
+                        <iframe class="border-0 pt-2" src="https://www.youtube.com/embed/qHMMSkLm24I"
+                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
+                    <hr class="d-xl-none d-sm-block d-md-none">
+                </div>
+                <div class="col-12 col-md-4">
+                    <h3 class="titulosDelFooter float-md-right">Sitios relacionados</h3>
+                    <ul class="d-flex flex-row float-md-right" id="sitiosRelacionados">
+                        <li>
+                            <a href="https://www.facebook.com/uid.unitec/">
+                                <img src="img/LogoUNITEC.png" class="logoDeSitiosRelacionados" alt="Logo del laboratorio Unitec">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/LogoEDETEC.png" class="logoDeSitiosRelacionados" alt="Logo del laboratorio de Edetec">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/LogoLATE.png" class="logoDeSitiosRelacionados" alt="Logo del laboratorio de Late">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <small class="float-right">Labtic-Unitec. 2019</small>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="<?php bloginfo('languaje'); ?>">
+<head>
+            <meta charset="<?php bloginfo('charset'); ?>">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="description" content="<?php bloginfo('description'); ?>">
+    <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/boostrap/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/normalize.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <title>Labtic</title>
+</head>
+
+<body>
+
+    <!-- Encabezado y menú de navegación / Header and navbar -->
+    <header class="container-fluid shadow p-3 mb-3 bg-white rounded">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <a href="index.html"><img class="logo float-left p-3" src="img/logo.png" alt="Logo de labtic"></a>
+                    <h1><strong>LabTIC&nbsp;-&nbsp;Unitec</strong></h1>
+                    <p class="text-secondary">Laboratorio de hardware y software de la facultad de ingeniería de la
+                        UNLP.</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <nav class="navbar navbar-expand-lg">
+                        <ul class="nav nav-pills flex-nowrap">
+                            <li class="nav-item">
+                                <a href="index.html" class="nav-link active">
+                                    <i class="fa fa-fw fa-home fa-lg"></i>Inicio
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="mapa-del-sitio.html" class="nav-link">
+                                    <i class="fa fa-fw fa-globe fa-lg"></i>Mapa del sitio
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="contacto.html" class="nav-link">
+                                    <i class="fa fa-fw fa-lg -o fa-envelope"></i>Contacto
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
