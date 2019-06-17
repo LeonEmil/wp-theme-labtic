2b3262d833765ee43e6384884dae3497ead929fe
Leon Emil <LeonEmilioBernal@gmail.com>
2019-06-17 11:49:44 -0300

    add template files

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+printf('
+    <div class="container">
+        <h1>Error 404, archivo no encontrado.</h1>
+
+        <a href="home_url()"><p class="text-primary mt-5 mb-5"><i class="fa fa-w fa-glove fa-lg"></i>Ir al mapa del sitio</p></a>
+        <a href="home_url()"><p class="text-primary mt-5 mb-5"><i class="fa fa-w fa-home fa-lg"></i>Volver a la página de inicio</p></a>
+
+    </div>
+');
+get_footer();
