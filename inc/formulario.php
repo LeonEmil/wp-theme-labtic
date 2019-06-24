2f045133628de9461295f1bd9d49c7c59b900819
Leon Emil <LeonEmilioBernal@gmail.com>
2019-06-24 19:39:34 -0300

    modified formulario.php

--- inc/formulario.php
+++ inc/formulario.php
@@ -7,7 +7,7 @@ function captura_de_valores_del_formulario(){
     $telefono = sanitize_text_field( $_POST['telefono']);
     $mensaje = sanitize_text_field( $_POST['mensaje']);
 
-    wp_mail("LeonEmilioBernal@gmail.com", "Formulario de contacto", $mensaje . " Numero de telefono: " .$telefono . " Email: " .$email);
+    wp_mail("jose.visionuno@gmail.com", "Formulario de contacto", $mensaje . " Numero de telefono: " .$telefono . " Email: " .$email);
     wp_redirect(add_query_arg(array('exito' => '1' ), get_home_url() . "/contacto" ));exit;
 }
 
